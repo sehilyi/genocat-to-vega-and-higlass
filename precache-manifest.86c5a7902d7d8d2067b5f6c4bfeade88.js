987fae13cbe24080d6bfa88c071151d4b2a4019e
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-05-04 14:37:48 -0400

    Updates

---
+++ precache-manifest.86c5a7902d7d8d2067b5f6c4bfeade88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b553d40f2c2deb1aa11a75f1c9e219a5",
+    "url": "/genocat-to-vega-and-higlass/index.html"
+  },
+  {
+    "revision": "ec9f14ef1911776cdf80",
+    "url": "/genocat-to-vega-and-higlass/static/css/main.5491e906.chunk.css"
+  },
+  {
+    "revision": "2c646e16dfcde9e00b7e",
+    "url": "/genocat-to-vega-and-higlass/static/js/2.f6d1a86a.chunk.js"
+  },
+  {
+    "revision": "06323e1b5363e387cb43ec8865edcd79",
+    "url": "/genocat-to-vega-and-higlass/static/js/2.f6d1a86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9f14ef1911776cdf80",
+    "url": "/genocat-to-vega-and-higlass/static/js/main.11a81cb2.chunk.js"
+  },
+  {
+    "revision": "5c03fa651291f32a7dc8",
+    "url": "/genocat-to-vega-and-higlass/static/js/runtime-main.3b988c5c.js"
+  }
+]);
